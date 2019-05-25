5422ea7ff8179354ef4a257452e4294e74573ccb
ScionSpy <29105063+ScionSpy@users.noreply.github.com>
2019-05-25 13:11:32 -0700

    Begining the Repo for the SRPG

---
+++ srpg.js
@@ -0,0 +1,10 @@
+/**
+ * Shadow RPG
+ * Created by: {HSF}ScionSpy#1904
+ * Backed by: {HSF}Bejebajay#1904
+ * May 25'th, 2019
+ */
+
+
+const collect = require('./collections.js').execute(bot);
+collect;
